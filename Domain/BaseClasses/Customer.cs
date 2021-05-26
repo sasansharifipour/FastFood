f64e4e7df42be8c2f955f5c253c21bbaa417fcc7
Sasan Sharifipour <sasansharifipour@gmail.com>
2021-05-26 13:35:56 +0430

    add customer and food

---
+++ Domain/BaseClasses/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Domain.BaseClasses
+{
+    public class Customer
+    {
+        public int ID { get; set; } = 0;
+
+        public string Name { get; set; } = "";
+
+        public string Family { get; set; } = "";
+
+        public string Mobile { get; set; } = "";
+
+        public string Address { get; set; } = "";
+    }
+}
---
+++ Domain/BaseClasses/Food.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Domain.BaseClasses
+{
+    public class Food
+    {
+        public int ID { get; set; } = 0;
+
+        public string Name { get; set; } = "";
+
+        public double Price { get; set; } = 0;
+    }
+}
