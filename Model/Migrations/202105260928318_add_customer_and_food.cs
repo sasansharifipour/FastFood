ffb80535e9f777019f7439a5ca19794f00c8f33d
Sasan Sharifipour <sasansharifipour@gmail.com>
2021-05-26 13:59:25 +0430

    add connection string and add migration for customer and food

---
+++ Model/Migrations/202105260928318_add_customer_and_food.cs
@@ -0,0 +1,40 @@
+﻿namespace Model.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class add_customer_and_food : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Family = c.String(),
+                        Mobile = c.String(),
+                        Address = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.Foods",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Price = c.Double(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Foods");
+            DropTable("dbo.Customers");
+        }
+    }
+}
