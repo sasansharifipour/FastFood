807b14aaf8565a982c6442521114eb3b1cff6584
Sasan Sharifipour <sasansharifipour@gmail.com>
2021-06-02 14:54:06 +0430

    ADD UNIT and Ingredient dao

---
+++ DTO/IngredientDAO.cs
@@ -0,0 +1,49 @@
+﻿using DAO;
+using Domain.BaseClasses;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DAO
+{
+    public interface IIngredientDAO : IBaseDAO<Ingredient>
+    {
+
+    }
+
+    public class IngredientDAO : IIngredientDAO
+    {
+        private IDbContextFactory<DBContext> _db_factory;
+        private IBaseDAO<Ingredient> _crud_operator;
+
+        public IngredientDAO(IDbContextFactory<DBContext> db_factory, IBaseDAO<Ingredient> crud_operator)
+        {
+            _db_factory = db_factory;
+            _crud_operator = crud_operator;
+        }
+
+        public bool Add(Ingredient data)
+        {
+            return _crud_operator.Add(data);
+        }
+
+        public bool Delete(Ingredient data)
+        {
+            return _crud_operator.Delete(data);
+        }
+
+        public IEnumerable<Ingredient> Select(Expression<Func<Ingredient, bool>> filter)
+        {
+            return _crud_operator.Select(filter);
+        }
+
+        public bool Update(Ingredient data)
+        {
+            return _crud_operator.Update(data);
+        }
+    }
+}
---
+++ DTO/UnitDAO.cs
@@ -0,0 +1,49 @@
+﻿using DAO;
+using Domain.BaseClasses;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DAO
+{
+    public interface IUnitDAO : IBaseDAO<Unit>
+    {
+
+    }
+
+    public class UnitDAO : IUnitDAO
+    {
+        private IDbContextFactory<DBContext> _db_factory;
+        private IBaseDAO<Unit> _crud_operator;
+
+        public UnitDAO(IDbContextFactory<DBContext> db_factory, IBaseDAO<Unit> crud_operator)
+        {
+            _db_factory = db_factory;
+            _crud_operator = crud_operator;
+        }
+
+        public bool Add(Unit data)
+        {
+            return _crud_operator.Add(data);
+        }
+
+        public bool Delete(Unit data)
+        {
+            return _crud_operator.Delete(data);
+        }
+
+        public IEnumerable<Unit> Select(Expression<Func<Unit, bool>> filter)
+        {
+            return _crud_operator.Select(filter);
+        }
+
+        public bool Update(Unit data)
+        {
+            return _crud_operator.Update(data);
+        }
+    }
+}
