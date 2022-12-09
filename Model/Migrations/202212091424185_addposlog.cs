3d323625a6c11f9b6f497ef9bba5ce9ad6e2f17c
Sasan Sharifipour <Sasansharifipour@gmail.com>
2022-12-09 18:00:53 +0330

    add migration pos

---
+++ Model/Migrations/202212091424185_addposlog.cs
@@ -0,0 +1,39 @@
+﻿namespace Model.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addposlog : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.PosTransactionResults",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        OrderID = c.Int(nullable: false),
+                        Deleted = c.Boolean(nullable: false),
+                        SoftwareExecutionTime = c.DateTime(nullable: false),
+                        TranType = c.Int(nullable: false),
+                        ErrorCode = c.Int(nullable: false),
+                        ErrorMsg = c.String(),
+                        PaymentAmount = c.String(),
+                        RRN = c.String(),
+                        Stan = c.String(),
+                        DateTime = c.String(),
+                        MerchantId = c.String(),
+                        TerminalId = c.String(),
+                        CardNumber = c.String(),
+                        MessageId = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.PosTransactionResults");
+        }
+    }
+}
