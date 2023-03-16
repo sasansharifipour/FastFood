4e8573cef240871cd0abdc7ad8e7ec158bacec4b
Sasan Sharifipour <Sasansharifipour@gmail.com>
2023-03-17 02:19:59 +0330

    migrate the database

--- Domain/BaseClasses/Food.cs
+++ Domain/BaseClasses/Food.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace Domain.BaseClasses
 {
-    public class Food
+    public class Food : ILogicalDeleteable
     {
         public int ID { get; set; } = 0;
 
---
+++ Model/Migrations/202303162248171_deleteable.cs
@@ -0,0 +1,20 @@
+﻿namespace Model.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class deleteable : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.ConsumeFoodOptions", "Deleted", c => c.Boolean(nullable: false));
+            AddColumn("dbo.Consumes", "Deleted", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Consumes", "Deleted");
+            DropColumn("dbo.ConsumeFoodOptions", "Deleted");
+        }
+    }
+}
